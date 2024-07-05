69b2340b90b6e9ea26483492473e485bdea0e0a8
Kene Lin <kenelin@gmail.com>
2024-07-05 18:09:24 +0800

    Add more CMake options

--- src/mosquitto_broker_internal.h
+++ src/mosquitto_broker_internal.h
@@ -26,7 +26,11 @@ Contributors:
 #ifdef WITH_WEBSOCKETS
 #  include <libwebsockets.h>
 #  if LWS_LIBRARY_VERSION_NUMBER >= 3002000 && !defined(LWS_WITH_EXTERNAL_POLL)
-#    warning "libwebsockets is not compiled with LWS_WITH_EXTERNAL_POLL support. Websocket performance will be unusable."
+#    if defined(_MSC_VER)
+#      pragma message("libwebsockets is not compiled with LWS_WITH_EXTERNAL_POLL support. Websocket performance will be unusable.")
+#    else
+#      warning "libwebsockets is not compiled with LWS_WITH_EXTERNAL_POLL support. Websocket performance will be unusable."
+#    endif
 #  endif
 #endif
 
